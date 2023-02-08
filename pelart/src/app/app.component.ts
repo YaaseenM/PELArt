458fa1ca09b24af62e88386ceca4a328ddb17bee
djm18 <djmeyer18@gmail.com>
2023-02-08 15:47:25 -0500

    HTTP
    Imported Http client and added functionality to upload files given an api

--- pelart/src/app/app.component.ts
+++ pelart/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({ // Known as the 'root component' - required.
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pelart';
+
+  constructor(
+    private http:HttpClient
+  ){}
+  name:string="";
+  file:any=null;
+  getName(name:string){
+    this.name=name;
+  }
+
+  getFile(event:any){
+    this.file=event.target.files[0];
+    console.log('file', this.file);
+  }
+
+  submitData(){
+      let fd=new FormData();
+      fd.set("name",this.name)
+      fd.set("file",this.file)
+
+      //Doesnt work, requires api from backend
+      this.http.post('http://localhost:4200/fileupload', fd).subscribe(
+        (response)=>{
+
+        }
+
+      )
+  }
 }
