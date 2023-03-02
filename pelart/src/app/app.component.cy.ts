9a92ae6e055b8bc2c3f77ad0e59f72575c3b59dd
djm18 <djmeyer18@gmail.com>
2023-03-01 23:44:17 -0500

    Cleaning up some files
    Added some cypress testing

--- pelart/src/app/app.component.cy.ts
+++ pelart/src/app/app.component.cy.ts
@@ -3,7 +3,9 @@ import { HttpClientModule } from '@angular/common/http'
 describe('AppComponent', () => {
   it('mounts', () => {
     cy.mount(AppComponent, {
-        imports: [HttpClientModule]
+        imports: [HttpClientModule],
+
     })
+    cy.get('[type="file"]').should('have.text', '')
   })
 })
