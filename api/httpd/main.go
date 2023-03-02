cf91fbf57efec5ec59d40535cd110c9c7925a718
YaaseenM <mohammad.yaaseen@ufl.edu>
2023-03-01 22:49:47 -0500

    Added GoRestAPI

---
+++ api/httpd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "GoRestAPI/httpd/handler"
+  "GoRestAPI/platform/newsfeed"
+
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+  feed := newsfeed.New()
+  r := gin.Default()
+  
+  r.GET("/ping", handler.PingGet())
+  r.GET("/newsfeed", handler.NewsfeedGet(feed))
+  r.POST("/newsfeed", handler.NewsfeedPost(feed))
+
+  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+
+
+}
