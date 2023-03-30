a23b88558ac3521b8c9ecbde98be0fee4353120d
ryanh6900 <houstonr@ufl.edu>
2023-03-29 21:53:14 -0400

    Removed Mongadb credentials.

--- UploadImages/src/config/db.js
+++ UploadImages/src/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "mongodb+srv://ryan:admin123@artworks.fhe8n6r.mongodb.net/?retryWrites=true&w=majority",
+  url: "mongodb+srv://ryan:<password>@artworks.fhe8n6r.mongodb.net/?retryWrites=true&w=majority",
   database: "Arts",
   imgBucket: "photos",
 };
