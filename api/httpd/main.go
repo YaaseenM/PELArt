ea76030411f9ce436149ab50ce4db67e32c057f2
YaaseenM <92874301+YaaseenM@users.noreply.github.com>
2023-04-17 21:32:20 -0400

    Updated with image list and patch

--- api/httpd/main.go
+++ api/httpd/main.go
@@ -3,7 +3,6 @@ package main
 import (
   "GoRestAPI/httpd/handler"
   "GoRestAPI/platform/newsfeed"
-
   "github.com/gin-gonic/gin"
 )
 
@@ -11,12 +10,14 @@ func main() {
   feed := newsfeed.New()
   r := gin.Default()
   
-  r.GET("/ping", handler.PingGet())
-  r.GET("/newsfeed", handler.NewsfeedGet(feed))
-  r.POST("/newsfeed", handler.NewsfeedPost(feed))
+  api :=r.Group("/api")
+  {
+  api.GET("/ping", handler.PingGet())
+  api.GET("/newsfeed", handler.NewsfeedGet(feed))
+  api.POST("/newsfeed", handler.NewsfeedPost(feed))
+  api.PATCH("/newsfeed/:fullname", handler.NewsfeedPatch(feed))
 
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-
-
+  }
 
 }
